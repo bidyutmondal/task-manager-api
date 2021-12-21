c4149824a496a1cda4562f3c04446b652dda1bfd
Bidyut Mondal <bidyutm007@gmail.com>
2021-12-21 11:52:09 +0530

    message added conn to db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+    console.log('Connected to database');
+})
