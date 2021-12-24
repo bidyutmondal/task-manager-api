1374603c5834996de479b76bba6024448810bd00
Bidyut Mondal <bidyutm007@gmail.com>
2021-12-24 13:47:10 +0530

    testing part added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
-    console.log('Connected to database');
+    //console.log('Connected to database');
 })
